7c64b1ca9c4e1634c093838a823610aff37c5f62
Leonidas <ldbrooks297@gmail.com>
2021-02-09 13:33:42 -0800

    added toys and associations

--- dbplayground.js
+++ dbplayground.js
@@ -75,21 +75,71 @@ const db = require('./models')
 
 //find particular user, then logs all pets associated with that user.
 
-db.user.findOne({ where: {id: 1 }})
-.then(user=>{
-    user.createPet({
-        name: 'Sawyer',
-        species: 'Dog'
-    })
-    .then(newPet=>{
-        console.log(newPet)
-    })
-})
+// db.user.findOne({ where: {id: 1 }})
+// .then(user=>{
+//     user.createPet({
+//         name: 'Sawyer',
+//         species: 'Dog'
+//     })
+//     .then(newPet=>{
+//         console.log(newPet)
+//     })
+// })
 
-db.user.findOne({ where: {id: 1}})
-.then(user=>{
-    user.getPets()
-    .then(allPets=>{
-        console.log(allPets)
-    })
-})
+// db.user.findOne({ where: {id: 1}})
+// .then(user=>{
+//     user.getPets()
+//     .then(allPets=>{
+//         console.log(allPets)
+//     })
+// })
+
+// db.pet.findOrCreate({
+//     where: {
+//       name: "Poopsie",
+//       species: "German Shepherd"
+//     }
+//   }).then(function([pet, created]) {
+//     // Second, get a reference to a toy.
+//     db.toy.findOrCreate({
+//       where: {type: "squeaky ball", color: "fuschia"}
+//     }).then(function([toy, created]) {
+//       // Finally, use the "addModel" method to attach one model to another model.
+//       pet.addToy(toy).then(function(relationInfo) {
+//         console.log(toy.type, "added to", pet.name);
+//       });
+//     });
+//   });
+
+// db.toy.findOne({
+//     where: {type: "ball"
+//     }
+// }).then(function(toy){
+//     toy.getPets().then(function(pets){
+//         console.log(pets.length, " pet(s) love the ", toy.color, toy.type)
+//     })
+// })
+
+// db.toy.findOrCreate({
+//     where: {type: "cornucopia", color: "brown"}
+// }).then(function([toy, created]){
+//     toy.getPets()
+//     .then(function(pets){
+//         if(pets.length > 0){
+//             pets.forEach(function(pet){
+//                 console.log(`${pet.name} loves their ${toy.color}, ${toy.type}`)
+//             })
+//         } else {
+//             db.pet.findOrCreate({
+//                 where: {
+//                     name: "Harold",
+//                     species: "Hamster"
+//                 }
+//             }).then(function([pet, created]){
+//                 toy.addPet(pet).then(function(relationInfo){
+//                     console.log(`${pet.name} has faved the ${toy.color}, ${toy.type} toy.`)
+//                 })
+//             }) 
+//         }
+//     })
+// })
